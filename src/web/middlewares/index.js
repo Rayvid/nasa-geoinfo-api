3f153ab8cf0f8858f9835866f4053dc0dd381d32
g.kisonas <g.kisonas@ba.lt>
2019-10-19 16:11:11 +0300

    add CORS headers

--- src/web/middlewares/index.js
+++ src/web/middlewares/index.js
@@ -4,9 +4,19 @@ const modelInitializer = require('./modelInitializer');
 const bodyParser = require('body-parser');
 const errorHandler = require('./error.js');
 const notFoundHandler = require('./404.js');
+const cors = require('cors');
 
 module.exports = {
   beforeHandler: [
+    app => app.options('*', cors()),
+    app => app.use('*', (req, res, next) => {
+      //replace localhost to the address of your server
+      res.header("Access-Control-Allow-Origin", "http://localhost");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
+      res.header('Access-Control-Allow-Credentials', true);
+      next();
+    }),
     app => app.use(expressWinston.logger(log)),
     app => app.use(bodyParser.json({ limit: '10mb' })),
     modelInitializer],
