6e8769d20135cf4f5da4b4d33b6a290946ec67f0
g.kisonas <g.kisonas@ba.lt>
2019-10-20 04:24:40 +0300

    add possibility to specify full url

--- src/web/routes/clickhouseToGeojson.js
+++ src/web/routes/clickhouseToGeojson.js
@@ -2,7 +2,10 @@ const fetch = require('node-fetch');
 
 module.exports = {
   fetchGeoJson: async (req, res) => {
-    let chResult = await (await fetch(`http://34.252.164.246:8123/?query=${req.query.query}%20FORMAT%20JSON`)).json();
+    let chResult = await (await fetch(
+      req.query.url
+        ? req.query.url
+        : `http://34.252.164.246:8123/?query=${req.query.query}%20FORMAT%20JSON`)).json();
     let result = {
       "type": "FeatureCollection",
       "features": chResult.data.map((value) => {
